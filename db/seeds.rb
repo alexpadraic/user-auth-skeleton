6b7049d8aced57255a7bde55d89c7905f7895834
Renan Barbosa e Souza & Tolsen Wilmot <pair+renanba.tolsen2484@devbootcamp.com>
2016-07-16 16:16:58 -0700

    Add one more Deck to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,12 @@
 cu = Deck.create(name: 'Curiosity')
 Card.create(question: 'What is the circumference of Earth at the equator', answer: '24,874 ', deck_id: cu.id)
-
-cu = Deck.create(name: 'Curiosity')
 Card.create(question: 'What means USA', answer: 'United State', deck_id: cu.id)
-
-cu = Deck.create(name: 'Curiosity')
 Card.create(question: 'Which continent where USA', answer: 'North America', deck_id: cu.id)
-
-
-
 
 user = User.create(email: 'eu@gmail.com', name: 'renan', password_hash: '123')
 
 
+second_deck = Deck.create(name: 'Nature')
+Card.create(question: 'Where is Yosemite park?', answer: 'California', deck_id: second_deck.id)
+Card.create(question: 'Where is Yellowstone?', answer: 'Wyoming', deck_id: second_deck.id)
+Card.create(question: 'Where is Grand Canyon', answer: 'Arizona', deck_id: second_deck.id)
