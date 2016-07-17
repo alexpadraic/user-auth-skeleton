9b0f40eb8c1a507c24e95030239ffc33746f9d24
Eric Booker & nick bushell <pair+ericbooker12.nbushell4977@devbootcamp.com>
2016-07-16 18:13:07 -0700

    Add routes for homepage and going into a game.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,24 +8,21 @@ get '/homepage' do
   erb :'homepage/index'
 end
 
+get '/decks/:deck_id/cards/:card_id' do
+  @current_deck = Deck.find(params[:deck_id])
+  sessions[:cards] = Card.where(deck_id: params[:deck_id])
+  @current_card = @cards[(params[:card_id].to_i-1)]
+  @current_card_id = params[:card_id].to_i-1
+  # session[:in_game] = true
 
-
- get '/decks/:deck_id/card/:card_id' do
-  @cards = Card.all
-   @card = Card.find_by(deck_id: params[:deck_id])
-   erb :decks
- end
-
-# post '/rounds' do
-#   erb :rounds
-#   redirect "/cards/#{params[:card]}"
-# end
-
-get '/cards' do
-  erb :cards
+  erb :'deck/index'
 end
 
+post '/decks/:deck_id/cards/:card_id' do
 
-get '/results' do
-  redirect "/"
+  if params[:user_guess] == Card.find(params[:card_id]).answer
+
+  # session[:in_game] = true
+
+  erb :'deck/index'
 end
