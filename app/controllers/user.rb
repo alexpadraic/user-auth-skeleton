80f06d05bb75d8437327095422f82cbc874ffb3e
Georgia Rust & TakiVlasakakis <pair+grust101.takivlasakakis@devbootcamp.com>
2016-07-16 17:08:38 -0700

    Write routes and form for sign-up :sparkle:

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,1 +1,18 @@
-# get 'user/new'
+get '/users/new' do
+
+erb :'/users/new'
+end
+
+
+post '/users' do
+@user = User.new(params[:user])
+
+if @user.save
+  redirect "/sessions/new"
+else
+  @error = "Sorry try again"
+  erb :'/users/new'
+end
+
+
+end
