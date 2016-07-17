be8155f82306193107b8dba64507f8cd415956b0
Eric Booker & nick bushell <pair+ericbooker12.nbushell4977@devbootcamp.com>
2016-07-16 17:12:50 -0700

    initial commit

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,2 +1,23 @@
 get '/' do
+  erb :index
+end
+
+ get '/decks/:deck_id/card/:card_id' do
+  @cards = Card.all
+   @card = Card.find_by(deck_id: params[:deck_id])
+   erb :decks
+ end
+
+# post '/rounds' do
+#   erb :rounds
+#   redirect "/cards/#{params[:card]}"
+# end
+
+get '/cards' do
+  erb :cards
+end
+
+
+get '/results' do
+  redirect "/"
 end
