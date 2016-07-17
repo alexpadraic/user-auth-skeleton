71d01ede91483a350c794f39cfb800ca4b115f74
Alexander Pellas <email": null>
2016-07-17 15:18:58 -0700

    Complete game logic and routing.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,7 +5,6 @@ end
 get '/homepage' do
   p params
   p session
-  session[:card_ids_in_play] = nil
   session[:round_info] = nil
   @decks = Deck.all
 
@@ -16,7 +15,7 @@ get '/decks/:deck_id/cards/:card_id' do
 
   @current_deck = Deck.find(params[:deck_id])
   @current_card = Card.find(params[:card_id])
-  session[:card_ids_in_play] ||= @current_deck.cards.map(&:id)
+
 
   # if session[:card_ids_in_play].length == 0
   #   erb :'rounds/show'
@@ -24,6 +23,8 @@ get '/decks/:deck_id/cards/:card_id' do
 
   if session[:round_info].nil?
     session[:round_info] = {}
+    session[:round_info][:incorrect_cards] ||= []
+    session[:round_info][:card_ids_in_play] ||= @current_deck.cards.map(&:id)
     session[:round_info][:guesses] ||= 0
     session[:round_info][:correct] ||= 0
     session[:round_info][:deck_id] ||= @current_deck.id
@@ -33,11 +34,12 @@ get '/decks/:deck_id/cards/:card_id' do
 
   if params[:user_guess] == @current_card.answer
     @result_response = "Correct! The answer is: #{@current_card.answer}"
-    session[:card_ids_in_play].delete(@current_card.id)
+    session[:round_info][:card_ids_in_play].delete(@current_card.id)
     session[:round_info][:guesses] += 1
     session[:round_info][:correct] += 1
   elsif params[:user_guess].nil? == false
     session[:round_info][:guesses] += 1
+    session[:round_info][:incorrect_cards] << @current_card.id if session[:round_info][:incorrect_cards].exclude?@current_card.id
     @result_response = "WRONG! The answer was: #{@current_card.answer}"
   end
 
@@ -50,6 +52,6 @@ end
 get '/rounds/show' do
 
 
-erb :'rounds/show'
+  erb :'rounds/show'
 end
 
