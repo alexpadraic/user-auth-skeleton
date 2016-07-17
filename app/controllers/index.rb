880edb2a5d3c664a96693a003207d790faca8ce7
raj7desai <raj7desai@gmail.com>
2016-07-16 17:04:51 -0700

    Some styling for ERBs and a couple of routes setup to test

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,2 +1,13 @@
 get '/' do
+  erb :index
+end
+
+get '/homepage' do
+  @decks = Deck.all
+  erb :'homepage/index'
+end
+
+get '/deck/:id' do
+  @cards=Card.find_by(deck_id: params[:id])
+  erb :'deck/index'
 end
