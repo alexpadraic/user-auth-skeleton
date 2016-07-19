95fead2871afb407dccc33dfefd03a25ff2d6cb9
Alexander Pellas <email": null>
2016-07-19 08:34:36 -0700

    Renamed some files and cleaned up the spacing a little.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
-cu = Deck.create(name: 'Curiosity')
-Card.create(question: 'What is the circumference of Earth at the equator', answer: '24,874 ', deck_id: cu.id)
-Card.create(question: 'What means USA', answer: 'United State', deck_id: cu.id)
-Card.create(question: 'Which continent where USA', answer: 'North America', deck_id: cu.id)
+cu = Deck.create(name: 'Geography')
+Card.create(question: 'What is the circumference of Earth at the equator?', answer: '24,874', deck_id: cu.id)
+Card.create(question: 'What is the name of the biggest ocean on earth?', answer: 'Pacific Ocean', deck_id: cu.id)
+Card.create(question: 'Which continent is the USA located in?', answer: 'North America', deck_id: cu.id)
 
 user = User.create(email: 'eu@gmail.com', name: 'renan', password: '123')
 
 
 second_deck = user.decks.create(name: 'Nature')
 
-second_deck.cards.create(question: 'Where is Yosemite park?', answer: 'California', deck_id: second_deck.id)
-second_deck.cards.create(question: 'Where is Yellowstone?', answer: 'Wyoming', deck_id: second_deck.id)
-second_deck.cards.create(question: 'Where is Grand Canyon', answer: 'Arizona', deck_id: second_deck.id)
+second_deck.cards.create(question: 'What state is Yosemite Park located in?', answer: 'California', deck_id: second_deck.id)
+second_deck.cards.create(question: 'What state is Yellowstone Park located in?', answer: 'Wyoming', deck_id: second_deck.id)
+second_deck.cards.create(question: 'What state is the Grand Canyon located in?', answer: 'Arizona', deck_id: second_deck.id)
 
 5.times do
   User.create(email: Faker::Internet.email, name: Faker::Name.name, password: '123')
